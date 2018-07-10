e1ee15e9c7e2753e3e6ee308ede14e4f7f3c8409
1 <lulin1@xiaomi.com>
2018-07-10 11:28:40 +0800

    Add unique-array

---
+++ unique-array/unique-array.js
@@ -0,0 +1,18 @@
+/* Method one : ES6 */
+var uniqueOne = (a) => [...new Set(a)];
+
+/* Method two : indexOf */
+var uniqueTwo = function (arr) {
+	var uniArr = [];
+	for (var i = 0; i < arr.length; i++) {
+		var current = arr[i];
+		if (uniArr.indexOf(current) === -1) {
+			uniArr.push(current);
+		}
+	}
+	return uniArr;
+}
+
+var array = [1, 9, 9, 2, 0, 8, 1, 6];
+console.log('Method one result : ' + uniqueOne(array));
+console.log('Method two result : ' + uniqueTwo(array));
