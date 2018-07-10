4bdb6373360c3be622c3be86527ad628e496a659
lulin <lulin1@xiaomi.com>
2018-07-10 11:31:58 +0800

    Update unique-array.js

--- unique-array/unique-array.js
+++ unique-array/unique-array.js
@@ -14,5 +14,5 @@ var uniqueTwo = function (arr) {
 }
 
 var array = [1, 9, 9, 2, 0, 8, 1, 6];
-console.log('Method one result : ' + uniqueOne(array));
-console.log('Method two result : ' + uniqueTwo(array));
+console.log('Method one result : ' + uniqueOne(array)); //[1, 9, 2, 0, 8, 6]
+console.log('Method two result : ' + uniqueTwo(array)); //[1, 9, 2, 0, 8, 6]
