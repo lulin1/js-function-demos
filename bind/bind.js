5dd3ce8db0784b8e631e4de932aa2eb390692f23
1 <lulin1@xiaomi.com>
2018-08-02 19:31:30 +0800

    Add notes

--- bind/bind.js
+++ bind/bind.js
@@ -1,3 +1,5 @@
+ /*bind() 方法会创建一个新函数。当这个新函数被调用时，bind() 的第一个参数将作为它运行时的 this，之后的一序列参数将会在传递的实参前传入作为它的参数。(来自于 MDN )*/
+
  // if (!Function.prototype.bind) {
  	Function.prototype.bind = function (oThis) {
  		if (typeof this !== 'function') {
