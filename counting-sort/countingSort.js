272f0c36a1cb8dd8e3f6964a8018154dc56975cf
1 <lulin1@xiaomi.com>
2018-09-11 11:25:31 +0800

    Add annotation and readme.md

--- counting-sort/countingSort.js
+++ counting-sort/countingSort.js
@@ -4,19 +4,19 @@ var countingSort = function (arr, maxValue) {
 		bucketLen = maxValue + 1,
 		sortedIndex = 0,
 		arrLen = arr.length;
-	for(var i = 0; i < arrLen; i++) {
+	for(var i = 0; i < arrLen; i++) { //将arr[i]的值作为bucket的key（键）
 		if (!bucket[arr[i]]) {
 			bucket[arr[i]] = 0;
 		}
-		bucket[arr[i]]++;
+		bucket[arr[i]]++; //计算arr[i]在bucket中(作为bucket的key)出现的次数
 		console.log(bucket);
 	}
 	for(var j = 0; j < bucketLen; j++) {
-		while(bucket[j] > 0) {
+		while(bucket[j] > 0) { //如果bucket的key（j）对应的值（即对key的计数）大于0
 			arr[sortedIndex] = j;
 			console.log('arr[sortedIndex]==='+arr[sortedIndex])
 			sortedIndex++;
-			bucket[j]--;
+			bucket[j]--; //对同一个key的计数有多少个就赋值给arr多少次
 		}
 	}
 	return arr;
